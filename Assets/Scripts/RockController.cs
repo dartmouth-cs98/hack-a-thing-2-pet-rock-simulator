b830e69000c079f62feddad9774a62c22f60cb91
Abby Starr <astarr2015@gmail.com>
2019-01-14 20:12:29 -0500

    can choose a rock now

---
+++ Assets/Scripts/RockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // MyRock = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            transform.Rotate(new Vector3(0, 15, 0) * Time.deltaTime);
+        }
+    }
+}
