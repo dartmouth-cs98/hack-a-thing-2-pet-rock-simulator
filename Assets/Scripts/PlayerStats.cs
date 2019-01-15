ec5d87f00cee6afdb1e0a81de99d40d7dc70f23b
Abby Starr <astarr2015@gmail.com>
2019-01-14 23:39:24 -0500

    can now save rock name and choice

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats 
+{
+    private static string rockName;
+    private static string typeOfRock;
+    private static ArrayList accessories = new ArrayList();
+
+    public static string RockName
+    {
+        get
+        {
+            return rockName;
+        }
+        set
+        {
+            rockName = value;
+        }
+    }
+
+    public static string TypeOfRock
+    {
+        get
+        {
+            return typeOfRock;
+        }
+        set
+        {
+            typeOfRock = value;
+        }
+    }
+
+    public static ArrayList Accessories
+    {
+        get
+        {
+            return accessories;
+        }
+        set
+        {
+            accessories = value;
+        }
+    }
+
+
+
+
+}
