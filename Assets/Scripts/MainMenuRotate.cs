840220dfce4c8754f0b2810a1445b459fe4a21d4
Andrew Alini <andrewalini97@gmail.com>
2019-01-14 21:06:01 -0500

    Main Menu with Rock Rotation

---
+++ Assets/Scripts/MainMenuRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // MyRock = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, -15, 0) * Time.deltaTime);
+        
+    }
+}
--- Assets/Scripts/Start.cs
+++ Assets/Scripts/Start.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-
+//Adapted from https://www.youtube.com/watch?v=5x2tDagmFiQ
 public class Start : MonoBehaviour
 {
     // Start is called before the first frame update
