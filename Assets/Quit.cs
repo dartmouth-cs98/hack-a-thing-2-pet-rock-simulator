84e3540123247ee32221383ba83eff3ac2e5d2c3
Andrew Alini <andrewalini97@gmail.com>
2019-01-14 20:23:10 -0500

    Main Menu-will need rotating rock

---
+++ Assets/Quit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//Code adapted from https://www.youtube.com/watch?v=4-X1FDylROA
+public class Quit : MonoBehaviour
+{
+    public void toQuit()
+    {
+        Debug.Log("Player has Quit Game");
+        Application.Quit();
+    }
+
+}
