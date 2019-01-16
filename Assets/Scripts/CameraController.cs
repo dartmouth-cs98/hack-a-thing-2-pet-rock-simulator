116ab9d73431ae45388ffd11cc6bf6c5333f6752
Andrew Alini <andrewalini97@gmail.com>
2019-01-16 02:01:20 -0500

    Simulator works with instantiation of class, will need to add label of rock name and hook up other scenes.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        string type = PlayerStats.TypeOfRock;
+        //string type = "rock_medium";
+        if (type == "rock_small")
+        {
+            player = GameObject.Find("rock_small_prefab(Clone)");
+
+        }
+        else if (type == "rock_medium")
+        {
+            player = GameObject.Find("rock_medium_prefab(Clone)");
+
+        }
+        else if (type == "rock_large")
+        {
+            player = GameObject.Find("rock_large_prefab(Clone)");
+
+        }
+
+        transform.position = player.transform.position + offset;
+    }
+}
